588d880a6b7827739ef3c19c7dd2e68fa3c657f9
Stephen Karpinskyj <stephenkarpinskyj@scarletcitystudios.com>
2017-06-19 21:01:27 +1200

    Box moving randomly each tick

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Box : MonoBehaviour
+{
+    [SerializeField]
+    private Transform visualParent;
+
+    private float size;
+    private float halfSize;
+
+    private void Awake()
+    {
+        this.size = this.transform.localScale.x;
+        this.halfSize = this.size / 2f;
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnTick += this.HandleTick;
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Exists)
+        {
+            GameManager.Instance.OnTick -= this.HandleTick;
+        }
+    }
+
+    private void Move(float duration, Vector2 dir)
+    {
+        Debug.Log("[Box] Moving dir=" + dir + ", duration=" + duration, this);
+
+        var visualPos = this.visualParent.position;
+        var pivotPos = visualPos;
+        var addedPivotRot = Vector3.zero;
+
+        if (!Mathf.Approximately(dir.x, 0f))
+        {
+            pivotPos.x += Mathf.Sign(dir.x) * this.halfSize;
+            addedPivotRot.z -= Mathf.Sign(dir.x) * 90f;
+        }
+        else if (!Mathf.Approximately(dir.y, 0f))
+        {
+            pivotPos.z += Mathf.Sign(dir.y) * this.halfSize;
+            addedPivotRot.x += Mathf.Sign(dir.y) * 90f;
+        }
+
+        pivotPos.y = 0f;
+
+        this.transform.position = pivotPos;
+        this.visualParent.position = visualPos;
+
+        this.transform.DORotate(addedPivotRot, duration, RotateMode.WorldAxisAdd)
+            .SetEase(Ease.InQuad);
+    }
+
+    private void MoveRandom(float duration)
+    {
+        var dir = Vector2.zero;
+
+        switch (Random.Range(0, 4))
+        {
+            case 0: dir.x += 1f; break;
+            case 1: dir.x -= 1f; break;
+            case 2: dir.y += 1f; break;
+            case 3: dir.y -= 1f; break;
+        }
+
+        this.Move(duration, dir);
+    }
+
+    private void HandleTick()
+    {
+        this.MoveRandom(0.5f);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : BehaviourSingleton<GameManager>
+{
+    private const float TickDuration = 1f;
+    private const int TickCount = 10;
+
+    public delegate void OnTickDelegate();
+    public OnTickDelegate OnTick = delegate { };
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += this.HandleSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= this.HandleSceneLoaded;
+    }
+
+    private void Start()
+    {
+        this.Reset();
+    }
+
+    private void Reset()
+    {
+        this.StopAllCoroutines();
+
+        this.StartCoroutine(this.RunCoroutine());
+    }
+
+    private IEnumerator RunCoroutine()
+    {
+        Debug.Log("[Game] Starting", this);
+
+        yield return new WaitForSeconds(1f);
+
+        for (var i = 0; i < TickCount; i++)
+        {
+            Debug.Log("[Game] Tick=" + i, this);
+            this.OnTick();
+
+            yield return new WaitForSeconds(TickDuration);
+        }
+
+        Debug.Log("[Game] Finished", this);
+    }
+
+    private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        this.Reset();
+    }
+}
