8a172fb38516efced65942ac35ebbd939dc10aa7
Stephen Karpinskyj <stephenkarpinskyj@scarletcitystudios.com>
2017-06-25 20:21:31 +1200

    Add reflection + modify lighting

---
+++ Assets/Scripts/RealtimeReflection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RealtimeReflection : MonoBehaviour
+{
+    [SerializeField]
+    private ReflectionProbe probe;
+
+    private void Update()
+    {
+        this.probe.transform.position = new Vector3(
+            Camera.main.transform.position.x, 
+            Camera.main.transform.position.y * -1, 
+            Camera.main.transform.position.z
+        );
+
+        this.probe.RenderProbe();
+    }
+}
